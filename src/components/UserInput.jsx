4d5f75dde621f8199bb1b56d4ef95702f0bd4123
Jakub <havier1993@gmail.com>
2023-11-28 21:38:04 +0100

    add initial value to input

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -27,7 +27,7 @@ export default function UserInput({}) {
 			<div className='input-group'>
 				<p>
 					<label>initial investment</label>
-					<input type='number' required onChange={handleInitial} />
+					<input type='number' required onChange={handleInitial} value={initialInvestment} />
 				</p>
 				<p>
 					<label>annual investment</label>

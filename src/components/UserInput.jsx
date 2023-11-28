23a954d02d3605ef672d09d42804ad066e2666f9
Jakub <havier1993@gmail.com>
2023-11-28 21:30:27 +0100

    added useState

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,4 +1,13 @@
+import {useState} from 'react'
 export default function UserInput({}) {
+
+   const [initialInvestment, setInitialInvestment] = useState(20000)
+   const [annualInvestment, setAnnualInvestment] = useState(20000)
+   const [expectedDuration, setEpectedDuration] = useState(20000)
+   const [duration, setDuration] = useState(20000)
+
+
+
 	return (
 		<section id='user-input'>
 			<div className='input-group'>

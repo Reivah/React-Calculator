ad4b7d390e5953c1ec88e8889df8eecacca5e3fd
Jakub <havier1993@gmail.com>
2023-11-28 21:20:28 +0100

    adding jsx to user component

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -5,7 +5,12 @@ export default function UserInput({}) {
 	return (
 		<section id='user-input'>
 			<div className='input-group'>
-			
+            <label>initial investment<input type="number"></input></label>
+            <label>annual investment<input type="number"></input></label>
+			</div>
+			<div className='input-group'>
+            <label>expected return<input type="number"></input></label>
+            <label>duration<input type="number"></input></label>
 			</div>
 		
 			
